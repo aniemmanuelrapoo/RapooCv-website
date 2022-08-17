3e800e525a17ada1787603cab7eab59bd2d7f9e2
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-08-17 13:53:46 +0100

    show/hide faq answers

--- js/main.js
+++ js/main.js
@@ -3,3 +3,20 @@
 window.addEventListener('scroll', () => {
   document.querySelector('nav').classList.toggle('windows-scroll', window.scrollY > 0)
 })
+
+//show/hide faq answers
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+  faq.addEventListener('click', () => {
+    faq.classList.toggle('open')
+
+    //change icon to minus
+    const icon = faq.querySelector('.faq__icon i')
+    if(icon.className === 'uil uil-plus'){
+      icon.className = "uil uil-minus"
+    }else{
+      icon.className = "uil uil-plus"
+    }
+  })
+})
